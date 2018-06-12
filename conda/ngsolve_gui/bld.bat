f48b5cf21eb42d6072782a3217e7c36ac994758a
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2018-06-12 17:42:51 +0200

    install dir

--- conda/ngsolve_gui/bld.bat
+++ conda/ngsolve_gui/bld.bat
@@ -3,7 +3,7 @@ cd build
 
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release ^
        -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-       -DCMAKE_INSTALL_PREFIX=%SP_DIR%/ngsolve ^
+       -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
        -DCMAKE_SYSTEM_PREFIX_PATH=%SYS_PREFIX% \
        %SRC_DIR%
 if errorlevel 1 exit 1
