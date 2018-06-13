b96e02c7403140f5d85b80a2c1f0a6ced3a941fc
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2018-06-13 11:28:07 +0200

    fix

--- conda/ngsolve_gui/bld.bat
+++ conda/ngsolve_gui/bld.bat
@@ -4,7 +4,7 @@ cd build
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release ^
        -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
        -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-       -DCMAKE_SYSTEM_PREFIX_PATH=%SYS_PREFIX% \
+       -DCMAKE_SYSTEM_PREFIX_PATH=%SYS_PREFIX% ^
        %SRC_DIR%
 if errorlevel 1 exit 1
 
