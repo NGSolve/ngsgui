067f6a6cfca742d08cab05e3b13c0a347a9d28d1
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2018-07-17 10:22:17 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 rm -r ~/.local/bin/ngsolve
+rm -r ~/.local/share/ngsgui
 rm -r ~/.local/lib/python3.6/site-packages/ngsgui*
 python3 setup.py install --user --single-version-externally-managed --root=/
