dc6548f31f42ce9212d234cd1a847200fdc4ef49
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2018-09-03 16:34:25 +0200

    check if epc is installed when selecting emacs editor

--- src/globalSettings.py
+++ src/globalSettings.py
@@ -10,12 +10,23 @@ class SettingDialog(QtWidgets.QDialog):
         self._editorCB.addItems(["default","emacs"])
         settings = QtCore.QSettings()
         self._editorCB.setCurrentText(settings.value("editor/type", "default"))
+        self._editorCB.currentTextChanged.connect(self._checkValidSelection)
         self._saveBtn = QtWidgets.QPushButton("Save")
         self._saveBtn.clicked.connect(self._save)
         self._cancelBtn = QtWidgets.QPushButton("Cancel")
         self._cancelBtn.clicked.connect(self.close)
         self.setLayout(ArrangeV(ArrangeH(QtWidgets.QLabel("Editor:"), self._editorCB),
                                 ArrangeH(self._cancelBtn, self._saveBtn)))
+
+    def _checkValidSelection(self, text):
+        if text == "emacs":
+            try:
+                import epc
+            except ModuleNotFoundError:
+                self._editorCB.setCurrentText("default")
+                self.msgbox = QtWidgets.QMessageBox(text="""Cannot embed emacs without epc, please install it with
+pip3 install --user epc""")
+                self.msgbox.show()
 
     def _save(self):
         settings = QtCore.QSettings()
