4888ff76afe278d4c45238a3f9aad9935ddd034e
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2018-09-13 16:52:48 +0200

    add missing file...

---
+++ src/jupyter.py
@@ -0,0 +1,13 @@
+from IPython import get_ipython
+ipython = get_ipython()
+ipython.magic('gui qt5')
+import time
+
+import ngsgui.gui as G
+import ngsolve as ngs
+from ngsgui.__main__ import Draw,Redraw
+
+ngs.Draw = Draw
+ngs.Redraw = Redraw
+G.gui = G.GUI(['-noOutputpipe', '-noConsole'])
+G.gui._run(run_event_loop=False)
