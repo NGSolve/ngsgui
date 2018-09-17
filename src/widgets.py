d92a4062dc0e044393036d14d9bc9825cfc79585
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2018-09-17 10:54:09 +0200

    Better looking margins

--- src/widgets.py
+++ src/widgets.py
@@ -35,8 +35,8 @@ def Arrange(layout_type, *args):
             layout.addWidget(w)
         else:
             layout.addLayout(w)
-    #layout.setSpacing(0)
-    layout.setContentsMargins(0,0,0,0)
+    m = 5
+    layout.setContentsMargins(5,5,5,5)
     return layout
 
 def ArrangeV(*args):
