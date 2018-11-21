cdd9149cae1aec65fabda6123a30d671eeb5f9d9
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2018-11-21 15:39:01 +0100

    use pyqt5 by default

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from os import environ as _environ
-_environ["QT_API"] = "pyside2"
+# _environ["QT_API"] = "pyside2"
+# _environ["QT_API"] = "pyqt5"
 if 'NGS_DEBUG' in _environ:
     import OpenGL
     OpenGL.FULL_LOGGING = True
