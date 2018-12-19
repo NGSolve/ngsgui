111338870f1587b62202bb5537c5eeba46991fe7
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2018-12-19 11:09:45 +0100

    remove install script

--- install.sh
+++
@@ -1,4 +0,0 @@
-rm -r ~/.local/bin/ngsolve
-rm -r ~/.local/share/ngsgui
-rm -r ~/.local/lib/python3.6/site-packages/ngsgui*
-python3 setup.py install --user --single-version-externally-managed --root=/
