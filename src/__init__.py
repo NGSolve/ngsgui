18e6f09cfa99bd849d8b9c75631bf01ebc5fe815
Christoph Wintersteiger <c.wintersteiger@gmx.at>
2019-04-16 11:49:59 +0200

    fixes loading issues of matplotlib when pyside2 is used

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,13 @@
 from os import environ as _environ
 # _environ["QT_API"] = "pyside2"
 # _environ["QT_API"] = "pyqt5"
+# needed when pyside2 is used, order in which qt is loaded seems to be important
+# otherwise crash in setting.py:856 when loading the matplotlib colormaps
+try:
+    import matplotlib.pyplot as plt
+except:
+    pass
+
 if 'NGS_DEBUG' in _environ:
     import OpenGL
     OpenGL.FULL_LOGGING = True
