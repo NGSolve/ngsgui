d7b79eff0627b13f37778c7a301e4674016fb43a
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2019-04-17 13:29:18 +0200

    add pyside2 dependency if pyqt5 cannot be found

--- setup.py
+++ setup.py
@@ -12,6 +12,13 @@ dirs = { module : module.replace('ngsgui.','src/') if 'ngsgui.' in module else '
 modules += ['spyder_ngsgui']
 dirs['spyder_ngsgui'] = 'src/spyder_ngsgui'
 
+dependencies = ["PyOpenGL", "psutil", "qtconsole>=4.4.0", "numpy",
+                "matplotlib>=2.2.3", "qtpy>=1.5.2", "ipykernel<5"]
+try:
+    import PyQt5
+except ImportError:
+    dependencies += ["PySide2"]
+
 setup(name="ngsgui",
       version="0.1.10",
       description="New graphical interface for NGSolve",
@@ -26,7 +33,6 @@ setup(name="ngsgui",
                    "Environment :: X11 Applications :: Qt",
                    "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)"),
       extras_require = {'test': ["pytest-qt"] },
-      install_requires=["PyOpenGL", "psutil", "qtconsole>=4.4.0", "numpy",
-                        "matplotlib>=2.2.3", "qtpy>=1.5.2", "ipykernel<5"],
+      install_requires=dependencies,
       entry_points={ "console_scripts" : "ngsolve = ngsgui.__main__:main" })
 
